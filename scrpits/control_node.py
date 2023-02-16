7ed1ec9f12ac729b0c3451f36cc3ebfd8590e051
P13LIAM <petong.zuckerberg@gmail.com>
2023-02-16 16:12:06 +0700

    fix: path

--- scrpits/control_node.py
+++ scrpits/control_node.py
@@ -12,8 +12,8 @@ from geometry_msgs.msg import Twist
 import sys
 cwd = os.getcwd()
 
-DATA_PATH = '/home/palm/PycharmProjects/ros2_ql/Data'
-MODULES_PATH = '/home/palm/PycharmProjects/ros2_ql/scripts'
+DATA_PATH = '/root/Scamper/ros2_ql/Data'
+MODULES_PATH = '/root/Scamper/ros2_ql/scripts'
 sys.path.insert(0, MODULES_PATH)
 
 from Qlearning import *
